b561bf4d6927308696a7a2d7029390ed07ab0860
calebcrens <thor.godulus@gmail.com>
2018-07-24 10:44:12 -0700

    added pictures

---
+++ loop.js
@@ -0,0 +1,16 @@
+var animals = ['zebra','lion','tiger','elephant','monkey'];
+var largestAnimalname= animals.filter(animal=> animal.length ==4).
+var capitalize= professions.map(profession=>profession.toUpperCase());
+var awesome= students.map(student=>student+"is awesome")
+var total= numbers.reduce((memo,Currentvalue)=>memo+currentValue,O);
+
+
+
+var app= student.foreach{
+
+
+
+
+
+
+}
